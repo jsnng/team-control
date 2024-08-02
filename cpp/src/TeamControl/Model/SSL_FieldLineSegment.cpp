285d50beceba12055ce7ef042a733dfd977fe4a2
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-08-03 00:45:20 +1000

    remove cpp

--- cpp/src/TeamControl/Model/SSL_FieldLineSegment.cpp
+++
@@ -1,8 +0,0 @@
-#include "SSL_FieldLineSegment.hpp"
-
-SSL_FieldLineSegment::SSL_FieldLineSegment() {
-
-}
-SSL_FieldLineSegment::~SSL_FieldLineSegment() {
-    
-}
--- cpp/src/TeamControl/proto2/protoc.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/zsh
-
-for proto in `ls *.proto`; do
-    protoc --cpp_out=. ./$proto
-done
