c7087347ed1163e22c84a98c719b0801caa69c78
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-08-06 18:45:15 +1000

    ssl-reciever docstrings added

--- src/TeamControl/SSL_Receiver.cpp
+++ src/TeamControl/SSL_Receiver.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdexcept>
 
 #if defined(__unix__) || defined(__APPLE__)
 #include <fcntl.h>
@@ -11,36 +12,36 @@
 
 SSLReceiverBase::SSLReceiverBase(std::string group_ip_addr, uint32_t port) {
     std::cerr << "ip_addr: " << group_ip_addr << " port: " << port << std::endl;
-    set_ssl_addr(group_ip_addr, port);
+    if(!set_ssl_multicast_socket(group_ip_addr, port)) {
+        throw std::runtime_error("");
+    }
 }
 
-void
-SSLReceiverBase::set_ssl_addr(
+bool
+SSLReceiverBase::set_ssl_multicast_socket(
     std::string group_ip_addr,
     uint32_t port) {
+    // create a socket, issue ::setsockopt() for timeout/broadcasting
+    // and bind it.
     sockfd = ::socket(AF_INET, SOCK_DGRAM, 0);
     if(sockfd < 0) {
-        std::cerr << "Error opening socket. " << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Error opening socket.");
     }
 
     struct timeval timeout;
     timeout.tv_sec = 1;
     timeout.tv_usec = 0;
 
-    if(::setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
-        std::cerr << "Error setting socket SO_RCVTIMEO option." << std::endl;
-        exit(EXIT_FAILURE);
+    if(::setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeval)) < 0) {
+        throw std::runtime_error("Error setting socket SO_RCVTIMEO option.");
     }
 
     const int opt = 1;
-    if(::setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
-        std::cerr << "Error setting socket SO_REUSEADDR option." << std::endl;
-        exit(EXIT_FAILURE);
+    if(::setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(int)) < 0) {
+        throw std::runtime_error("Error setting socket SO_REUSEADDR option.");
     }
-    if(::setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt)) < 0) {
-        std::cerr << "Error setting socket SO_BROADCAST option." << std::endl;
-        exit(EXIT_FAILURE);
+    if(::setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(int)) < 0) {
+        throw std::runtime_error("Error setting socket SO_BROADCAST option.");
     }
 
     ::memset(&ssl_socket_addr, 0, sizeof(ssl_socket_addr));
@@ -48,35 +49,39 @@ SSLReceiverBase::set_ssl_addr(
     ssl_socket_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     ssl_socket_addr.sin_port = htons(port);
 
-    if(::bind(sockfd, reinterpret_cast<sockaddr*>(&ssl_socket_addr), sizeof(ssl_socket_addr)) < 0) {
+    if(::bind(sockfd, reinterpret_cast<sockaddr*>(&ssl_socket_addr), sizeof(sockaddr_in)) < 0) {
         ::close(sockfd);
-        std::cerr << "Error binding to socket: " << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Error binding to socket: ");
     }
     
+    // mutlicast join operation by issuing ::setsockopt().
+    // refer to linux socket documentation (see man ip 7) because
+    // struct `ip_mreq` has two forms for IP_ADD_MEMBERSHIP.
+
     struct ip_mreq group;
     if(::inet_pton(AF_INET, group_ip_addr.c_str(), &(group.imr_multiaddr)) < 0) {
-        std::cerr << "Error: group_ip_addr invalid." << std::endl;
-        exit(EXIT_FAILURE);
+        ::close(sockfd);
+        throw std::runtime_error( "Error: group_ip_addr invalid.");
     }
     group.imr_interface.s_addr = INADDR_ANY;
-    if(::setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &group, sizeof(group)) < 0) {
-        std::cerr << "Error setting socket to IP_ADD_MEMBERSHIP option." << std::endl;
-        exit(EXIT_FAILURE);
+    if(::setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &group, sizeof(ip_mreq)) < 0) {
+        ::close(sockfd);
+        throw std::runtime_error("Error setting socket to IP_ADD_MEMBERSHIP option.");
     }
+    return true;
 }
 
-std::string
+std::optional<std::string>
 SSLReceiverBase::receive_message() {
-    char buffer[SSL_RECV_MAX_BUFFER_SIZE];
+    char buffer[SSL_RECV_BUFFER_SIZE];
     struct sockaddr_in from_addr;
     socklen_t from_len = sizeof(from_addr);
 
-    ssize_t recv_bytes = recvfrom(sockfd, buffer, SSL_RECV_MAX_BUFFER_SIZE, 0, reinterpret_cast<sockaddr*>(&from_addr), &from_len);
+    ssize_t recv_bytes = recvfrom(sockfd, buffer, SSL_RECV_BUFFER_SIZE, 0, reinterpret_cast<sockaddr*>(&from_addr), &from_len);
     std::cerr << recv_bytes << std::endl;
-    if(recv_bytes > SSL_RECV_MAX_BUFFER_SIZE) {
+    if(recv_bytes > SSL_RECV_BUFFER_SIZE) {
         std::cerr << "Received packet too large";
-        return "";
+        return std::nullopt;
     }
     if(recv_bytes > 0) {
         buffer[recv_bytes] = '\0';
@@ -84,8 +89,9 @@ SSLReceiverBase::receive_message() {
     } 
     else if(errno != EAGAIN) {
         std::cerr << "No message received. Error: " << strerror(errno) << std::endl;
+        return std::nullopt;
     }
-    return "";
+    return std::nullopt;
 }
 
 SSLReceiverBase::~SSLReceiverBase() {
@@ -97,6 +103,9 @@ SSLReceiverBase::~SSLReceiverBase() {
     ::close(sockfd);
 }
 
+/**
+ * for testing purposes
+ */
 int
 main(int argc, char * argv[]) {
     // auto ssl_autoref_addr = SSLAutoRefReceiver::get_default_addr();
@@ -106,9 +115,7 @@ main(int argc, char * argv[]) {
     grSimVisionReceiver ssl_grsim_recv(std::get<0>(ssl_grsim_addr), std::get<1>(ssl_grsim_addr));
     while(true) {
         auto message = ssl_grsim_recv.receive_message();
-        std::cerr << message << std::endl;
+        std::cerr << message.value() << std::endl;
     }
-
-
     return EXIT_SUCCESS;
 }
--- src/TeamControl/SSL_Receiver.h
+++ src/TeamControl/SSL_Receiver.h
@@ -10,23 +10,47 @@
 
 #include <netinet/in.h>
 
-#define SSL_RECV_MAX_BUFFER_SIZE 2048
+#define SSL_RECV_BUFFER_SIZE 2048
 
+/**
+ * base ssl multicast receiver for ssl
+ * 
+ */
 class SSLReceiverBase {
     public:
-        SSLReceiverBase(std::string, uint32_t);
+        /**
+         * SSLReceieverBase constructor
+         * 
+         * @param group_ip_addr mutlicast group ip addr
+         * @param port mutlicast group port
+         * @throws runtime_error @see SSLReceiverBase::set_ssl_multicast_socket()
+         * 
+         */
+        SSLReceiverBase(std::string group_ip_addr, uint32_t port);
         ~SSLReceiverBase();
 
-        void set_ssl_addr(std::string, uint32_t);
-        // inline virtual std::tuple<std::string, uint32_t> get_default_addr() {
-        //     throw std::bad_function_call();
-        // };
-        // inline virtual void listen() {
-        //     throw std::bad_function_call();
-        // };
-        std::string receive_message();
+        /**
+         * listens to multicast on socket at descriptor `sockfd`
+         * 
+         * @see `SSL_RECV_BUFFER_SIZE` maximum size of recv buffer
+         * @returns std::optional<std::string> recieved messages from `sockfd` or std::nullopt
+         * 
+         */
+        std::optional<std::string> receive_message();
 
     private:
+        /**
+         * sets up a socket to for listening to the mutlicast group
+         * 
+         * @param group_ip_addr mutlicast group ip addr
+         * @param port mutlicast group port
+         * 
+         * @returns true if socket is set up successfully
+         * @throws runtime_error if socket setup fails
+         * 
+         * @todo change `group.imr_interface.s_addr` to `group_ip_addr` instead of `INADDR_ANY`
+         */
+        bool set_ssl_multicast_socket(std::string group_ip_addr, uint32_t port);
         struct sockaddr_in ssl_socket_addr;
         int sockfd = -1;
 };
@@ -36,6 +60,11 @@ class SSLVisionReceiver : public SSLReceiverBase {
         SSLVisionReceiver(std::string x, uint32_t y) : 
             SSLReceiverBase(x, y) {};
         ~SSLVisionReceiver() = default;
+        /**
+         * returns the default addr for listening to vision-ssl
+         * 
+         * @returns tuple<string, uint32_t> a tuple consisting of the group ip addr and port
+         */
         inline constexpr static std::tuple<std::string, uint32_t> 
         get_default_addr() {
             std::string ssl_vision_ip_addr = "224.5.23.2";
@@ -49,6 +78,11 @@ class SSLAutoRefReceiver : public SSLReceiverBase {
     public:
         SSLAutoRefReceiver(std::string x, uint32_t y) : 
             SSLReceiverBase(x, y) {};
+        /**
+         * returns the default addr for listening to autoref
+         * 
+         * @returns tuple<string, uint32_t> a tuple consisting of the group ip addr and port
+         */
         inline constexpr static std::tuple<std::string, uint32_t> 
         get_default_addr() {
             std::string ssl_autoref_ip_addr = "224.5.23.1";
@@ -62,6 +96,11 @@ class grSimVisionReceiver : public SSLReceiverBase {
     public:
         grSimVisionReceiver(std::string x, uint32_t y) : 
             SSLReceiverBase(x, y) {};
+        /**
+         * returns the default addr for listening to grSim mutlicast vision
+         * 
+         * @returns tuple<string, uint32_t> a tuple consisting of the group ip addr and port
+         */
         inline constexpr static std::tuple<std::string, uint32_t> 
         get_default_addr() {
             std::string ssl_grsim_ip_addr = "224.5.23.2";
