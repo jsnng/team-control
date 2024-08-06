96843f918893b590f1e675ddf86ae58e32189efa
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-08-07 08:21:43 +1000

    protobuf setup

--- src/protobuf/setup.sh
+++ src/protobuf/setup.sh
@@ -6,8 +6,10 @@ git clone https://github.com/abseil/abseil-cpp.git
 mkdir build/ && cd build
 cmake -DABSL_BUILD_TESTING=ON -DABSL_USE_GOOGLETEST_HEAD=ON -DCMAKE_CXX_STANDARD=14 ..
 cmake --build . --target all
+cmake --install .
 
 git clone https://github.com/protocolbuffers/protobuf.git
 cd protobuf
 git submodule update --init --recursive
-bazel build :protoc :protobuf
+cmake --build .
+cmake --install .
