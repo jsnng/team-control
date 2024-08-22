d89cba46cf494126a301939073e54191e5657185
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-08-23 08:02:09 +1000

    setup.py for python api

--- src/swig/setup.py
+++ src/swig/setup.py
@@ -1,1 +1,19 @@
 #! /usr/bin/env python
+
+from setuptools import setup, find_packages, Extension
+
+if __name__ == '__main__':
+    extension = Extension(
+        name = "swig",
+        sources = ["src/World_wrap.cxx"],
+        include_dirs = ["src/", "../protobuf", "../TeamControl"],
+        py_limited_api = True,
+    )
+
+    kwargs = dict(
+        packages = find_packages(where="src"),
+        package_dirs = {"": "src"},
+        ext_modules = [extension],
+    )
+    setuptools.setup(**kwargs)
+    
